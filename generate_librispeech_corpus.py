7ef226812bbe1fab1b99486d0fd1db33a2b8f7f1
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-12-22 14:45:59 +0800

    generate libsrispeech corpus for CrossASR

---
+++ generate_librispeech_corpus.py
@@ -0,0 +1,31 @@
+import os
+import numpy
+import glob
+import subprocess
+import helper
+
+def idx_to_file(idx):
+    return "/".join(idx.split("-")[:-1])
+
+if __name__ == "__main__":
+
+    root_dir = "LibriSpeech/test-clean/"
+
+    corpus_fpath = "LibriSpeech/test-clean-corpus.txt"
+
+    with open(corpus_fpath, 'w') as corpus_file:
+
+        ### root_dir needs a trailing slash (i.e. /root/dir/)
+        for filename in glob.iglob(root_dir + '**/*.trans.txt', recursive=True):
+            
+            file = open(filename)
+            for line in file.readlines():
+                idx = line.split()[0]
+                text = " ".join(line.split()[1:])
+
+                text = helper.preprocess_text(text)
+
+                corpus_file.write(text + "\n")
+
+            file.close()
+
